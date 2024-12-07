ad68007c0165fe9f6cd9801dee6fd05e27430c52
Aman Verma <aka.amanverma@gmail.com>
2024-12-07 12:51:18 +0530

    Update Grid.tsx

--- components/Grid.tsx
+++ components/Grid.tsx
@@ -1,10 +1,45 @@
-"use client"
+// "use client"
+// import { gridItems } from "@/data";
+// import { BentoGrid, BentoGridItem } from "./ui/BentoGrid";
+
+// const Grid = () => {
+//   return (
+//     <section >
+//       <BentoGrid className="w-full py-20">
+//         {gridItems.map((item, i) => (
+//           <BentoGridItem
+//             id={item.id}
+//             key={i}
+//             title={item.title}
+//             description={item.description}
+//             // remove icon prop
+//             // remove original classname condition
+//             className={item.className}
+//             img={item.img}
+//             imgClassName={item.imgClassName}
+//             titleClassName={item.titleClassName}
+//             spareImg={item.spareImg}
+//           />
+//         ))}
+//       </BentoGrid>
+//     </section>
+//   );
+// };
+
+
+// export default Grid;
+
+
+
+
+
+
 import { gridItems } from "@/data";
 import { BentoGrid, BentoGridItem } from "./ui/BentoGrid";
 
 const Grid = () => {
   return (
-    <section >
+    <section>
       <BentoGrid className="w-full py-20">
         {gridItems.map((item, i) => (
           <BentoGridItem
@@ -12,8 +47,6 @@ const Grid = () => {
             key={i}
             title={item.title}
             description={item.description}
-            // remove icon prop
-            // remove original classname condition
             className={item.className}
             img={item.img}
             imgClassName={item.imgClassName}
@@ -27,3 +60,5 @@ const Grid = () => {
 };
 
 export default Grid;
+
+
